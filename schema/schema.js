8ee5dd8fb0fcc1d416709a3e9e823e8c09012c35
Michael Tran <michaelhuytran@gmail.com>
2018-08-12 16:48:05 -0700

    [Feature] - EditUser

--- schema/schema.js
+++ schema/schema.js
@@ -84,10 +84,22 @@ const mutation = new GraphQLObjectType({
 		deleteUser: {
 			type: UserType,
 			args: {
-				userId: {type: new GraphQLNonNull(GraphQLString)},
+				id: {type: new GraphQLNonNull(GraphQLString)},
 			},
-			resolve(parentValue, {userId}) {
-				return axios.delete(`http://localhost:3000/users/${userId}`).then((res) => res.data);
+			resolve(parentValue, {id}) {
+				return axios.delete(`http://localhost:3000/users/${id}`).then((res) => res.data);
+			}
+		},
+		editUser: {
+			type: UserType,
+			args: {
+				id: {type: new GraphQLNonNull(GraphQLString)},
+				firstName: {type: GraphQLString},
+				age: {type: GraphQLInt},
+				companyId: {type: GraphQLString},
+			},
+			resolve(parentValue, args) {
+				return axios.patch(`http://localhost:3000/users/${args.id}`, args).then((res) => res.data);
 			}
 		}
 	}
